2fb9338f8b7bf28f8298afded910784646b0200a
RealKazbek <37765@iitu.edu.kz>
2026-05-13 15:37:13 +0500

    fix

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,3 +1,5 @@
+import path from 'node:path'
+
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
 
 /** @type {import('next').NextConfig} */
@@ -6,6 +8,14 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(process.cwd()),
+    }
+
+    return config
   },
   ...(basePath
     ? {
